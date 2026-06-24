73b5121c5cc94bd67bab2954e3148e316bec37ac
Mayank Goyal <mayankgoyal@Mayanks-MacBook-Air.local>
2026-06-24 17:03:41 +0530

    gmail ipv4 fix

--- server.js
+++ server.js
@@ -14,16 +14,15 @@ app.use(express.json());
 // ==========================
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 app.get("/", (req, res) => {
